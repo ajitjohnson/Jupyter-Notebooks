e566b00b1d8a6fbea124b988536232fd5f46c423
Ajit Johnson Nirmal <ajitjohnson.n@gmail.com>
2020-09-13 10:12:19 -0400

    update

--- r_scripts/gsva.R
+++ r_scripts/gsva.R
@@ -1,25 +1,101 @@
 # GSVA setup
 
-# Library
-library(GSEABase)
-library(GSVA)
 
+# load signature
+custom_sig <- read.csv(file= "/Users/aj/Dropbox (Partners HealthCare)/Data/covid19/signatures/stat3_sig.csv", row.names=1, header = F)
+custom_sig <- data.frame(t(custom_sig))
 
-# Convert dataframe colums to named list
-named_list <- function(df){
-  final_list <- c()
-  for (i in 1: ncol(df)){
-    tmp <- as.character(df[,i])
-    tmp <- list(tmp[nchar(tmp) > 1])
-    names(tmp) <- colnames(df[,i,drop=F])
-    final_list <- c(final_list, tmp)
+# Load data
+setwd("/Users/aj/Dropbox (Partners HealthCare)/Data/covid19/NHP")
+data <- read.csv(file= "exp_cleaned.csv", header = T, check.names=F,row.names = 1) #row.names = 1
+meta <- read.csv(file= "meta_cleaned.csv", row.names = 1, header = T,check.names=F)
+ndata <- read.csv(file= "ARSeq/normalized_data.csv", header = T, check.names=F, row.names = 1)
+
+# ssGSEA analysis 
+ssgsea_analysis <- function(data,signature,meta=NULL,ssgsea.norm=T, intgroup=NULL, custom_color=NULL, show_column_names=F, padding = unit(c(2, 2, 2, 60), "mm")){
+  
+  # load required libraries
+  require(GSEABase)
+  require(GSVA)
+  require(circlize)
+  require(ComplexHeatmap)
+  require(RColorBrewer)
+  
+  # Make a copy of the group type
+  group <- signature[,1,drop=FALSE]
+  colnames(group) <- c('cluster')
+  # Drop the groups from signature
+  signature <- signature[,-1]
+  # transpose the signature
+  signature <- data.frame(t(signature))
+  
+  # Convert signature into a named list
+  named_list <- function(df){
+    final_list <- c()
+    for (i in 1: ncol(df)){
+      tmp <- as.character(df[,i])
+      tmp <- list(tmp[nchar(tmp) > 1])
+      names(tmp) <- colnames(df[,i,drop=F])
+      final_list <- c(final_list, tmp)
+    }
+    return(final_list)
   }
-  return(final_list)
+  x = named_list (signature)
+  
+  # Run the enrichment
+  gbm_es <- gsva(as.matrix(data), x,method='ssgsea',ssgsea.norm=ssgsea.norm)
+  
+  # Viz using heatmap with metadata
+  ssgsea_scaled = t(scale(t(gbm_es)))
+  #h1_col = colorRamp2(c(-1, -0.5, 0, 0.5, 1), c("#4B6AAF",  '#55B0AE', "#F8F6B8","#F5A15B","#B11E4B"))
+  h1_col = colorRamp2(c(-1, 0, 1), c("#F8F6B8","#F5A15B","#B11E4B"))
+  #h1_col = colorRamp2(c(-1, -0.5, 0, 0.5, 1), c("#f2e9e4",  '#c9ada7', "#9a8c98","#4a4e69","#22223b"))
+  #h1_col = colorRamp2(c(-1, -0.5, 0, 0.5, 1), c("#e0e1dd",  '#778da9', "#415a77","#1b263b","#0d1b2a"))
+  #h1_col = colorRamp2(c(-1, -0.5, 0, 0.5, 1), c("#f8f9fa",  '#dee2e6', "#ced4da","#6c757d","#343a40"))
+  
+  # converge grouping with ssgsea
+  group <- group[row.names(group) %in% row.names(gbm_es), , drop = FALSE]
+  
+  if(!is.null(meta)){
+    # Add column annotation (Sample group)
+    col_ann <- data.frame(as.character(meta[,intgroup]))
+    colnames(col_ann) <- c('Groups')
+    # Create color pallete
+    colourCount <- length(unique(col_ann[,1]))
+    getPalette <- colorRampPalette(brewer.pal(8, "Dark2"))
+    col_color <- getPalette(colourCount)
+    if (!is.null(custom_color)){col_color <- custom_color}
+    names(col_color) <- unique(col_ann[,1])
+    col_color <- list(Groups = col_color)
+    col_Ann <- HeatmapAnnotation(df=col_ann, col=col_color, annotation_width=unit(c(1, 4), "cm"), gap=unit(1, "mm"))
+  }
+  
+  # Heatmap
+  if(is.null(meta)){
+    h1 <- Heatmap(as.matrix(ssgsea_scaled), col = h1_col, border=T, cluster_columns = T,  
+                  rect_gp = gpar(col = "#22223b", lwd = 1), show_column_names = show_column_names,
+                  row_names_gp = gpar(fontsize = 12), cluster_rows = T,
+                  row_split = factor(group$cluster, levels = unique(group$cluster)),
+                  show_row_names = TRUE, name='ssGSEA score')
+    draw(h1, heatmap_legend_side = "left", annotation_legend_side = "left", padding = padding)
+    
+  } else {
+    h1 <- Heatmap(as.matrix(ssgsea_scaled), col = h1_col, border=T, cluster_columns = F,  
+                  rect_gp = gpar(col = "#22223b", lwd = 1), show_column_names = show_column_names,
+                  row_names_gp = gpar(fontsize = 12), cluster_rows = T, top_annotation=col_Ann,
+                  row_split = factor(group$cluster, levels = unique(group$cluster)),
+                  show_row_names = TRUE, name='ssGSEA score')
+    draw(h1, heatmap_legend_side = "left", annotation_legend_side = "left", padding = padding)
+    
+  }
+  
 }
-x = named_list (custom_sig)
 
-# Run the enrichment
-gbm_es <- gsva(as.matrix(ndata), x,method='ssgsea',ssgsea.norm=T)
+# with meta
+ssgsea_analysis (data,meta,signature=custom_sig,intgroup="group")
+# without meta
+ssgsea_analysis (data,signature=custom_sig)
 
-# Viz using heatmap
-heatmap(gbm_es)
+
+
+
--- r_scripts/salmontocounts.R
+++ r_scripts/salmontocounts.R
@@ -4,6 +4,10 @@
 setwd("/Users/aj/Dropbox (Partners HealthCare)/Data/melanoma_rarecyte/round-5-f12-f9/f9/salmon/")
 setwd("/Users/aj/Dropbox (Partners HealthCare)/Data/melanoma_rarecyte/round-5-f12-f9/f12/salmon/")
 setwd("/Users/aj/Dropbox (Partners HealthCare)/Data/melanoma_rarecyte/round-1/salmon/")
+setwd("//research.files.med.harvard.edu/ImStor/sorger/data/rnaseq/ajit_johnson/covid/alignment/final")
+setwd("D:/gw/final")
+
+
 
 
 # Library
@@ -12,11 +16,16 @@ library("tximportData")
 library("EnsDb.Hsapiens.v86")
 library("stringr")
 
+
 # Load data
 #files <- file.path(getwd(), list.files(getwd()))
 files <- file.path(getwd(), list.files(pattern = ".sf", recursive = TRUE))
-  
-  
+
+# If needed
+tx2gene <- read.csv("2020-07-31_alignment/tx2gene.csv", header = F)
+files <- gsub("\\\\","//",files)
+files <- substring(files, 3)
+
 
 # Prepare to convert to gene
 txdf <- transcripts(EnsDb.Hsapiens.v86, return.type="DataFrame")
@@ -30,7 +39,7 @@ head(txi.tx$counts)
 # add column name
 file_names <- list.files(pattern = ".sf", recursive = TRUE)
 file_names <- str_remove(file_names, "/quant.sf")
-file_names <- str_remove(file_names, ".sf")
+file_names <- str_remove(file_names, "/salmon/quant.sf")
 
 # add columname
 data <- txi.tx$counts
@@ -38,8 +47,42 @@ colnames(data) <- file_names
 data <- round(data, digits = 0)
 
 # Write the data out
-write.csv(data, file = "counts.csv")
+write.csv(data, file = "counts_tpm.csv")
 
 # Convert transcripts to genes
-data <- arseq.ensembl2genename (data)
+d <- arseq.ensembl2genename (data)
+write.csv(data, file = "counts_tpm_genenames.csv")
 
+
+# Merge a lot of CSV files
+setwd("/Users/aj/Dropbox (Partners HealthCare)/Data/covid19/NHP/pickseq/fastq processing/viral/htseq-count/")
+library(dplyr)
+library(readr)
+library(tidyverse)
+d <- list.files(path=getwd(), full.names = TRUE) %>% lapply(read.table) %>% bind_cols
+row.names(d) <- d[,1]
+c <- 1:ncol(d)
+d <- d[, c%%2==0]
+# rename the columns
+file_names <- list.files(path=getwd(), full.names = TRUE)
+file_names <- substring(file_names, 101)
+file_names <- str_remove(file_names, ".counts")
+colnames(d) <- file_names
+
+write.csv(d, file = "counts.csv")
+
+# Functions
+arseq.ensembl2genename <- function(data,ensemblmirror=NULL){
+  require(biomaRt)
+  print("Converting ENSEMBL ID's to gene names")
+  ensembl <- useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", mirror=ensemblmirror)
+  genes <- getBM(attributes=c('ensembl_gene_id','hgnc_symbol'), mart = ensembl)
+  data_m <- merge(data, genes, by.x="row.names", by.y= "ensembl_gene_id")[,-1]
+  data_m <- data_m[!(data_m$hgnc_symbol==""), ]
+  # Reduce multiple transcripts into a single gene by taking the sum of related transcripts
+  data_m$hgnc_symbol <- as.factor(data_m$hgnc_symbol)
+  data <- aggregate(.~hgnc_symbol, data=data_m, sum)
+  rownames(data) <- data[,1]
+  data <- data[,-1]
+  return(data)
+}
